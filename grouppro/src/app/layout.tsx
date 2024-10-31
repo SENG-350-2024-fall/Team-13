13b6f83a0e73803a87a61628863a90e404f6f95b
aleksial <125409114+aleksial@users.noreply.github.com>
2024-10-31 11:20:37 -0700

    Add files via upload

---
+++ grouppro/src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { RoleProvider } from "@/contexts/RoleContext";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "ICU 360",
+  description: "Developed by Shaban, Aleksia, Shounit, Isaac and Arfaz",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <RoleProvider>
+          {children}
+        </RoleProvider>
+      </body>
+    </html>
+  );
+}
